08ebc61b203a4e720c889ff6832b9e824be263fa
fael-dutra <104550275+Rafael-Dutra-create@users.noreply.github.com>
2023-09-18 15:53:45 -0300

    #

--- front-end-project/src/Pages/Home.js
+++ front-end-project/src/Pages/Home.js
@@ -1,12 +1,25 @@
 import Letras from '../img/MsgNEnviada.gif'
 import '../Styles/Home.css'
 
+
 function Home(){
     return(
         <div>
-            <img alt="Mensagem Não Enviada" src={Letras} decoding="async" className='Letras'/>
-        </div>
-        
+            <div>
+                <img alt="Mensagem Não Enviada" src={Letras} decoding="async" className='Letras'/>
+            </div>
+
+            <div>
+                Campo de busca
+            </div>
+
+            <section className='container mx-auto.mt-8'>
+                <ul>
+                    
+                </ul>
+            </section>
+
+        </div>      
     )
 }
 
